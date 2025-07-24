fc58ff4ae32f53c697d9771514c75566cd274380
jckmssy <jacqueline.massaya@gov.scot>
2025-07-24 17:52:04 +0100

    using here

--- Training/training ui.R
+++ Training/training ui.R
@@ -6,10 +6,10 @@
 
 ### ------------------------------
 ### Load utility scripts
-source("utility/util_updates.R")
-source("utility/util_functions.R")
-source("utility/util_options.R")
-source("utility/hc_theme.R")
+source(here("utility", "util_updates.R"))
+source(here("utility", "util_functions.R"))
+source(here("utility", "util_options.R"))
+source(here("utility", "hc_theme.R"))
 ###------------------------------
 ### Load libraries
 # library(readxl)
@@ -23,23 +23,23 @@ library(highcharter)
 library(geojsonio)
 ###------------------------------
 ### Load scripts with functions used to create charts in module scripts
-source("charts_tables_functions/function_summary.R")
-source("charts_tables_functions/function_line_chart.R")
-source("charts_tables_functions/function_area_chart.R")
-source("charts_tables_functions/function_bar_chart.R")
-source("charts_tables_functions/function_percentage_bar_chart.R")
-source("charts_tables_functions/function_timelapse_bar_chart.R")
-source("charts_tables_functions/function_breakdown_chart.R")
-source("charts_tables_functions/function_data_table.R")
-source("charts_tables_functions/function_regions_map.R")
-source("charts_tables_functions/function_map.R")
-source("charts_tables_functions/function_multi_bar_chart.R")
+source(here("charts_tables_functions", "function_summary.R"))
+source(here("charts_tables_functions", "function_line_chart.R"))
+source(here("charts_tables_functions", "function_area_chart.R"))
+source(here("charts_tables_functions", "function_bar_chart.R"))
+source(here("charts_tables_functions", "function_percentage_bar_chart.R"))
+source(here("charts_tables_functions", "function_timelapse_bar_chart.R"))
+source(here("charts_tables_functions", "function_breakdown_chart.R"))
+source(here("charts_tables_functions", "function_data_table.R"))
+source(here("charts_tables_functions", "function_regions_map.R"))
+source(here("charts_tables_functions", "function_map.R"))
+source(here("charts_tables_functions", "function_multi_bar_chart.R"))
 ###-----------------------------
 ### Load training module
-source("Training/training module.R")
+source(here("Training", "training module.R"))
 ###-----------------------------
 ### Load census data 
-load("Data/census_data.Rdata")
+load(here("Data", "census_data.Rdata"))
 ###----------------------------
 ### Define main app ui and call in UI from training module
 
