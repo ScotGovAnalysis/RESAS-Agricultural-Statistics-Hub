6345be0be5a5ec8fa995e5ce551d8cf861298c51
Thomson K (Kirsty) <Kirsty.Thomson@gov.scot>
2025-08-13 11:28:15 +0100

    Adjusting input labels

--- Economy/TIFF/tiff_utility.R
+++ Economy/TIFF/tiff_utility.R
@@ -1,27 +1,24 @@
-tiff_year <- max(main_tiff_data_long$Year) #Current TIFF year
-tiff_year_min <- min(main_tiff_data_long$Year)
-
 tiff_Outputs <- c(
   "Gross Output" = "Gross_output",
-  "Crops" = "Total_output_from_crops",
-  "Livestock " = "Total_output_from_livestock",
+  "Crops output" = "Total_output_from_crops",
+  "Livestock output" = "Total_output_from_livestock",
   "Other Agricultural Output" = "Total_output_from_other_agricultural_activities",
   "Non-Agricultural Output" = "Total_output_from_non_agricultural_activities"
 )
 
 tiff_prices <- c("Current (nominal)",
-                 "Real terms")
+                 "Real terms (Constant 2024)")
 
 tiff_Inputs <- c(
   "Total Costs" = "Total_Costs",
-  "Seeds" = "Total_input_from_seeds",
-  "Feed" = "Total_input_from_feedstuffs",
-  "Fertilisers and Lime" = "Total_input_from_fertilisers_and_lime",
-  "Farm Maintenance" = "Total_input_from_farm_maintenance",
-  "Miscellaneous Expenses" = "Total_input_from_miscellaneous_expenses",
+  "Seeds input" = "Total_input_from_seeds",
+  "Feed input" = "Total_input_from_feedstuffs",
+  "Fertilisers and Lime input" = "Total_input_from_fertilisers_and_lime",
+  "Farm Maintenance input" = "Total_input_from_farm_maintenance",
+  "Miscellaneous Expenses input" = "Total_input_from_miscellaneous_expenses",
   "FISIM" = "FISIM",
-  "Non-Agricultural Input" = "Total_input_from_non_agricultural_activities",
-  "Gross Input" = "Gross_input",
+  "Non-Agricultural input" = "Total_input_from_non_agricultural_activities",
+  "Gross input" = "Gross_input",
   "Gross Value Added" = "Gross_value_added",
   "Consumption of Fixed Capital" = "Total_consumption_of_fixed_capital",
   "Net Value Added" = "Net_value_added",
