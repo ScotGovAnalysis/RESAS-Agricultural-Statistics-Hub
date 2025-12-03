32fe72856b1be4fbe96a67cea95c19f7cbf95ff2
Thomson K (Kirsty) <Kirsty.Thomson@gov.scot>
2025-12-03 14:31:33 +0000

    Processing oilseed data

--- Economy/TIFF/data_process_crops.R
+++ Economy/TIFF/data_process_crops.R
@@ -10,6 +10,17 @@ Cereals_census_data_long <- cereals_data %>%
          Year = as.integer(Year)
          )
 
+Oilseed_census_data_long <- oilseed_data %>% 
+  pivot_longer(
+    cols = -`Crop/Land use`,
+    names_to = "Year",
+    values_to = "Value"
+  ) %>% 
+  mutate(Measure = "Area",
+         `Crop/Land use` = as.factor(`Crop/Land use`),  
+         Measure = as.factor(Measure),
+         Year = as.integer(Year)
+  )
 
 
 cereals_tiff_data_path <- "//s0196a/ADM-Rural and Environmental Science-Farming Statistics/Agriculture/Source/TIFF/Cereals/"
@@ -44,3 +55,28 @@ cereals_combined_long <- bind_rows(Cereals_census_data_long, cereals_tiff_data_l
 
 #save to data
 save(cereals_combined_long, file="Data/Cereals_data.Rda")
+
+
+oilseed_tiff_data_long <- cereals_tiff_data %>% 
+  filter(!is.na(Year) & Year != "" & Year != 0) %>% 
+  mutate(Barley_Yield = as.numeric(Barley_Yield)) %>%
+  pivot_longer(
+    cols = -Year,   # keep Year (or other ID columns) as is
+    names_to = c("Crop/Land use", "Measure"),
+    names_pattern = "(.*)_(.*)",   # everything before last "_" = Crop, after = Measure
+    values_to = "Value"
+  ) %>% 
+  mutate(
+    `Crop/Land use` = as.factor(`Crop/Land use`),  
+    Measure = as.factor(Measure), 
+    `Crop/Land use` = recode(`Crop/Land use`,
+                             "OSR" = "Oilseed Rape Total"
+    )
+  ) %>% 
+  filter(`Crop/Land use` == "Oilseed Rape Total",
+         Measure != "Area")
+
+oilseed_combined_long <- bind_rows(Oilseed_census_data_long, oilseed_tiff_data_long)
+
+#save to data
+save(oilseed_combined_long, file="Data/Oilseed_data.Rda")
