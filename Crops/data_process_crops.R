7d9f00a6293e46f721fd6f07eb067a31a8739386
Thomson K (Kirsty) <Kirsty.Thomson@gov.scot>
2025-12-04 15:43:58 +0000

    Moving to crops folder

---
+++ Crops/data_process_crops.R
@@ -0,0 +1,92 @@
+main_cereals <- cereals_data %>%
+  filter(`Crop/Land use` %in% c("Wheat", "Barley Total", "Oats Total")) %>%
+  summarise(across(-`Crop/Land use`, ~ sum(.x, na.rm = TRUE))) %>%
+  mutate(`Crop/Land use` = "Main Cereals (Barley, Oats and Wheat)")
+
+# Bind back into the wide dataset
+cereals_data <- bind_rows(cereals_data, main_cereals)
+  
+
+Cereals_census_data_long <- cereals_data %>% 
+  pivot_longer(
+    cols = -`Crop/Land use`,
+    names_to = "Year",
+    values_to = "Value"
+  ) %>% 
+  mutate(
+    Measure = "Area",
+    `Crop/Land use` = as.factor(`Crop/Land use`),  
+    Measure = as.factor(Measure),
+    Year = as.integer(Year)
+  )
+
+Oilseed_census_data_long <- oilseed_data %>% 
+  pivot_longer(
+    cols = -`Crop/Land use`,
+    names_to = "Year",
+    values_to = "Value"
+  ) %>% 
+  mutate(Measure = "Area",
+         `Crop/Land use` = as.factor(`Crop/Land use`),  
+         Measure = as.factor(Measure),
+         Year = as.integer(Year)
+  )
+
+
+cereals_tiff_data_path <- "//s0196a/ADM-Rural and Environmental Science-Farming Statistics/Agriculture/Source/TIFF/Cereals/"
+file_path <- paste0(cereals_tiff_data_path, "CH_data_final.csv")
+
+cereals_tiff_data <- read.csv(file_path, stringsAsFactors = FALSE)
+
+cereals_tiff_data_long <- cereals_tiff_data %>% 
+  filter(!is.na(Year) & Year != "" & Year != 0) %>% 
+  mutate(Barley_Yield = as.numeric(Barley_Yield)) %>%
+  pivot_longer(
+    cols = -Year,   # keep Year (or other ID columns) as is
+    names_to = c("Crop/Land use", "Measure"),
+    names_pattern = "(.*)_(.*)",   # everything before last "_" = Crop, after = Measure
+    values_to = "Value"
+  ) %>% 
+  mutate(
+    `Crop/Land use` = as.factor(`Crop/Land use`),  
+    Measure = as.factor(Measure), 
+    `Crop/Land use` = recode(`Crop/Land use`,
+                  "S_Barley" = "Spring Barley",
+                  "W_Barley" = "Winter Barley",
+                  "Barley"  = "Barley Total",
+                  "Oats"  = "Oats Total",
+                  "Cereals" = "Main Cereals (Barley, Oats and Wheat)"
+                  )
+  ) %>% 
+  filter(`Crop/Land use` != "OSR",
+         Measure != "Area")
+  
+cereals_combined_long <- bind_rows(Cereals_census_data_long, cereals_tiff_data_long)
+
+#save to data
+save(cereals_combined_long, file="Data/Cereals_data.Rda")
+
+
+oilseed_tiff_data_long <- cereals_tiff_data %>% 
+  filter(!is.na(Year) & Year != "" & Year != 0) %>% 
+  mutate(Barley_Yield = as.numeric(Barley_Yield)) %>%
+  pivot_longer(
+    cols = -Year,   # keep Year (or other ID columns) as is
+    names_to = c("Crop/Land use", "Measure"),
+    names_pattern = "(.*)_(.*)",   # everything before last "_" = Crop, after = Measure
+    values_to = "Value"
+  ) %>% 
+  mutate(
+    `Crop/Land use` = as.factor(`Crop/Land use`),  
+    Measure = as.factor(Measure), 
+    `Crop/Land use` = recode(`Crop/Land use`,
+                             "OSR" = "Oilseed Rape Total"
+    )
+  ) %>% 
+  filter(`Crop/Land use` == "Oilseed Rape Total",
+         Measure != "Area")
+
+oilseed_combined_long <- bind_rows(Oilseed_census_data_long, oilseed_tiff_data_long)
+
+#save to data
+save(oilseed_combined_long, file="Data/Oilseed_data.Rda")
